42fdf20f166fa57dac5042b3a44ea434d7f192cd
Ana Coronel <a9nna.coronel@gmail.com>
2023-03-10 10:00:27 +0100

    Test UsersService with loginUser method

---
+++ src/app/services/users/users.service.spec.ts
@@ -0,0 +1,44 @@
+import { type HttpClient, type HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { type UserCredentials } from '../../user.model';
+import { UsersService } from "./users.service"
+
+describe('Given a UsersService class', () => {
+  let usersService: UsersService;
+  let httpMock: Partial<HttpClient>;
+  const {
+    apiUrl,
+    path: { users, loginUser },
+  } = environment;
+
+  beforeEach(() => {
+    jest.resetModules();
+    httpMock = {
+      post: jest.fn(),
+    };
+    usersService = new UsersService(httpMock as HttpClient);
+  });
+
+  describe('When we use it to create a new object called sevice', () => {
+
+    test('Then service must have loginUser method', () => {
+      expect(typeof usersService.loginUser).toBe('function');
+    });
+
+    test('Then it should call its loginUser method with the apiUrl, a user and an object with header property', () => {
+      const user: UserCredentials = {
+        email: "user@user.com",
+        password: 'holaholahola',
+      };
+      const api = `${apiUrl}${users}${loginUser}`;
+
+      usersService.loginUser(user);
+
+      expect(httpMock.post).toHaveBeenCalledWith(
+        api,
+        user,
+        expect.objectContaining({ headers: expect.any(Object) as HttpHeaders })
+      );
+    });
+  });
+});
