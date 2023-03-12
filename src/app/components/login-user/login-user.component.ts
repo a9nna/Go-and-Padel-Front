60a6d624621def12ec367b786ac8b78c0d4c6b85
Ana Coronel <a9nna.coronel@gmail.com>
2023-03-12 18:57:54 +0100

    Create error messages for LoginUserComponent

--- src/app/components/login-user/login-user.component.ts
+++ src/app/components/login-user/login-user.component.ts
@@ -1,8 +1,8 @@
 import { Component, Inject, InjectionToken } from '@angular/core';
-import { type FormGroup, FormBuilder } from '@angular/forms';
+import { type FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
 import { UsersService } from '../../services/users/users.service';
 import { type UserCredentials } from '../../../app/user.model';
-import { Store } from '@ngrx/store';
 import { loginUser } from '../../store/users/actions/user.actions';
 
 export const formBuilderToken = new InjectionToken<FormBuilder>('FormBuilder', {
@@ -14,19 +14,19 @@ export const formBuilderToken = new InjectionToken<FormBuilder>('FormBuilder', {
   templateUrl: './login-user.component.html',
   styleUrls: ['./login-user.component.scss'],
 })
-
 export class LoginUserComponent {
   loginForm: FormGroup = this.formBuilder.group({
-    email: [''],
-    password: [''],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
   });
+
+  loginFailed = false;
 
   constructor(
     @Inject(formBuilderToken) private readonly formBuilder: FormBuilder,
     @Inject(UsersService) private readonly usersService: UsersService,
     @Inject(Store) private readonly usersStore: Store
   ) {}
-
 
   onSubmit() {
     const userCredential = this.loginForm.value as UserCredentials;
@@ -36,7 +36,10 @@ export class LoginUserComponent {
 
       localStorage.setItem('token', token);
 
-      this.usersStore.dispatch(loginUser({user: {token}}))
-    });
+      this.usersStore.dispatch(loginUser({ user: { token } }));
+    }, () => {
+      this.loginFailed = true;
+    }
+    );
   }
 }
