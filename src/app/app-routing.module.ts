3d74e9af2c4969df66ceeb28fff3be0ffb2cd199
Ana Coronel <a9nna.coronel@gmail.com>
2023-03-14 23:40:31 +0100

    Create router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, type Routes } from '@angular/router';
+import { LoginUserComponent } from './components/login-user/login-user.component';
+import { HomePageComponent } from './components/pages/home-page/home-page.component';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginUserComponent },
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+  { path: 'home', redirectTo: '', pathMatch: 'full' },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
+import { Store, StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { HttpClientModule } from '@angular/common/http';
-import { UsersService } from './services/users/users.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginUserComponent } from './components/login-user/login-user.component';
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UsersService } from './services/users/users.service';
 import { reducer } from './store/users/reducers/user.reducer';
+import { AuthService } from './services/auth/auth.service';
+import { LoginUserComponent } from './components/login-user/login-user.component';
+import { UserModule } from './store/users/user.module';
+import { TokenService } from './services/token/token.service';
+import { HomePageComponent } from './components/pages/home-page/home-page.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginUserComponent],
+  declarations: [
+    AppComponent,
+    LoginUserComponent,
+    HomePageComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -22,11 +30,15 @@ import { reducer } from './store/users/reducers/user.reducer';
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
+    UserModule
   ],
   providers: [
     UsersService,
     FormBuilder,
+    Store,
     { provide: String, useValue: 'stringValue' },
+    AuthService,
+    TokenService
   ],
   bootstrap: [AppComponent],
 })
