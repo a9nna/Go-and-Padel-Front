cea64bdb3c9a7d353f1ef8c9bfe29a95078f2b25
Ana Coronel <a9nna.coronel@gmail.com>
2023-03-15 17:07:01 +0100

    Create matchFeature and set loadMatches action creator

---
+++ src/app/store/matches/reducers/matches.reducer.ts
@@ -0,0 +1,18 @@
+import { createFeature, createReducer, on } from '@ngrx/store';
+import { type Match } from 'src/app/match.model';
+import { loadMatches } from '../actions/matches.actions';
+
+const initialState: Match[] = [];
+
+export const matchesFeature = createFeature({
+  name: 'matches',
+  reducer: createReducer(
+    initialState,
+    on(
+      loadMatches,
+      (state, { matches }): Match[] => matches
+    )
+  )
+})
+
+export const {name, reducer, selectMatchesState} =  matchesFeature
