6cb9d6cfc528f1a4e2eebc6221d23b703f02e38c
Ana Coronel <a9nna.coronel@gmail.com>
2023-03-15 17:05:51 +0100

    Create loadMatches action creator

---
+++ src/app/match.model.ts
@@ -0,0 +1,9 @@
+export interface Match {
+  category: string,
+  date: Date,
+  level: string,
+  paddleCourt: number,
+  signedPlayersNumber: number,
+  image: string,
+  allowedPlayersNumber: number
+}
---
+++ src/app/store/matches/matches.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import { matchesFeature } from './reducers/matches.reducer';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    StoreModule.forFeature(matchesFeature),
+  ]
+})
+
+export class MatchesModule { }
