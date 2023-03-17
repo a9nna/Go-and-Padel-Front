5aaca12a76794322905683f0a9767f3279304624
Ana Coronel <a9nna.coronel@gmail.com>
2023-03-17 16:01:40 +0100

    Add deleteMatch action creator to matchesFeature

--- src/app/store/matches/reducers/matches.reducer.ts
+++ src/app/store/matches/reducers/matches.reducer.ts
@@ -1,14 +1,27 @@
+import { state } from '@angular/animations';
 import { createFeature, createReducer, on } from '@ngrx/store';
 import { type Match } from 'src/app/match.model';
-import { loadMatches } from '../actions/matches.actions';
+import { deleteMatch, loadMatches } from '../actions/matches.actions';
 
-const initialState: Match[] = [];
+const initialState: Match[] = [{
+  id: "",
+  category: "",
+  date: new Date(),
+  level: "",
+  paddleCourt: 1,
+  signedPlayersNumber: 0,
+  image: "",
+  allowedPlayersNumber: 0
+}];
 
 export const matchesFeature = createFeature({
   name: 'matches',
   reducer: createReducer(
     initialState,
-    on(loadMatches, (state, { matches }): Match[] => matches)
+    on(loadMatches, (state, { matches }): Match[] => matches),
+    on(deleteMatch, (state, { match }): Match[] => (state.filter(
+      (stateMatch) => stateMatch.id !== match.id
+    )))
   ),
 });
 
