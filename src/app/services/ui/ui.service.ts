6f1ab29ed315ada16db920964e94fc51ec30a2b8
Ana Coronel <a9nna.coronel@gmail.com>
2023-03-17 09:51:00 +0100

    Create a UiService and add methods showLoader and getLoader

--- src/app/services/ui/ui.service.ts
+++ src/app/services/ui/ui.service.ts
@@ -1,6 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { showLoading } from '../../store/ui/actions/ui.actions';
+import { selectIsLoading } from '../../store/ui/reducers/ui.reducers';
 
 @Injectable({
   providedIn: 'root',
 })
-export class UiService {}
+export class UiService {
+
+  constructor(@Inject(Store) private readonly store: Store){}
+
+  showLoader() {
+    this.store.dispatch(showLoading());
+  }
+
+  getLoader(){
+    this.store.select(selectIsLoading);
+  }
+}
