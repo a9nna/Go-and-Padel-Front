4b655bc46b8574822a172e38f3d8838892e7843b
Ana Coronel <a9nna.coronel@gmail.com>
2023-03-18 17:59:15 +0100

    Test deleteMatch method from MatchesComponent

--- src/app/components/matches/matches.component.spec.ts
+++ src/app/components/matches/matches.component.spec.ts
@@ -1,29 +1,38 @@
-import { render } from '@testing-library/angular';
+import { render, screen } from '@testing-library/angular';
+import { Store } from '@ngrx/store';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
 import { MatchesService } from '../../services/matches/matches.service';
 import { MockMatchesService } from '../../../mocks/MockMatchesService/MockMatchesService';
 import { MatchesComponent } from './matches.component';
 import { MatchComponent } from '../match/match.component';
+import mockStore from '../../../mocks/mockStore/mockStore';
+import mockMatches from '../../../mocks/mockMatches/mockMatches';
 
 const renderComponent = async () => {
   const matchesService = new MockMatchesService();
+  const store = mockStore();
+
   await render(MatchesComponent, {
     declarations: [ MatchComponent ],
+    componentProperties: {
+      match: mockMatches[0],
+    },
     providers: [
       { provide: MatchesService, useValue: matchesService },
+      { provide: Store, useValue: store}
     ],
   });
 
-  return { matchesService };
+  return { matchesService, store };
 };
 
 describe('Given a MatchesComponent', () => {
   describe('When its getMatches method is called', () => {
     test('Then its type is a function', async () => {
-      const { matchesService } = await renderComponent();
+      await renderComponent();
 
-      const matchesComponent = new MatchesComponent(
-        matchesService as unknown as MatchesService
-      );
+      const matchesComponent = MatchesComponent.prototype
 
       expect(typeof matchesComponent.getMatches).toBe('function');
     });
@@ -31,11 +40,9 @@ describe('Given a MatchesComponent', () => {
 
   describe('When its ngOnInit method is called', () => {
     test('Then its type is function', async () => {
-      const { matchesService } = await renderComponent();
+      await renderComponent();
 
-      const matchesComponent = new MatchesComponent(
-        matchesService as unknown as MatchesService
-      );
+      const matchesComponent = MatchesComponent.prototype
 
       expect(typeof matchesComponent.ngOnInit).toBe('function');
     });
@@ -52,4 +59,30 @@ describe('Given a MatchesComponent', () => {
       expect(matchesComponent.getMatches).toHaveBeenCalled();
     });
   });
+
+  describe("When its deleteMatch method is called", () => {
+    test("Then its type is 'function'", async() => {
+      await renderComponent();
+
+      const matchesComponent = MatchesComponent.prototype
+
+      expect(typeof matchesComponent.deleteMatch).toBe('function')
+    })
+  })
+
+  describe("When a button with 'Delete match' accessible text is clicked", () =>{
+    test("Then it should call its deleteMatch method", async() => {
+      const text = /delete match/i
+
+      await renderComponent()
+      const deleteMatchSpy = jest.spyOn(MatchesComponent.prototype, "deleteMatch")
+      const button = screen.getByRole("button", {
+        name: text
+      })
+
+      await userEvent.click(button)
+
+      expect(deleteMatchSpy).toHaveBeenCalled()
+    })
+  })
 });
