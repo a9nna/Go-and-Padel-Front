04e74926fb0ea87756873d5c2b605ac11ae776d4
Ana Coronel <a9nna.coronel@gmail.com>
2023-03-19 15:53:48 +0100

    Create CreateMatchComponent

---
+++ src/app/components/create-match/create-match.component.ts
@@ -0,0 +1,10 @@
+import { Component, Inject } from '@angular/core';
+
+@Component({
+  selector: 'app-create-match',
+  templateUrl: './create-match.component.html',
+  styleUrls: ['./create-match.component.scss'],
+})
+export class CreateMatchComponent {
+
+}
