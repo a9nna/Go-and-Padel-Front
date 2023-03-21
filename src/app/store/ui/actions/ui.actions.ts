37c655759beb2c3fdb12c52f3c976f65bca5bcc7
Ana Coronel <a9nna.coronel@gmail.com>
2023-03-21 15:04:08 +0100

    Create showErrorModal action creator

--- src/app/store/ui/actions/ui.actions.ts
+++ src/app/store/ui/actions/ui.actions.ts
@@ -7,3 +7,5 @@ export const hideLoading = createAction("[UI] Hide Loading");
 export const showModalSuccess = createAction("[UI] Show Success Modal");
 
 export const hideModal = createAction("[UI] Hide Modal");
+
+export const showModalError = createAction("[UI] Show Error Modal");
