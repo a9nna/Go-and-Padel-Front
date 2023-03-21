b52d794dc614712d23d5f331270002abc8395244
Ana Coronel <a9nna.coronel@gmail.com>
2023-03-21 10:36:06 +0100

    Create showModal action creator

--- src/app/store/ui/actions/ui.actions.ts
+++ src/app/store/ui/actions/ui.actions.ts
@@ -2,4 +2,6 @@ import { createAction } from "@ngrx/store";
 
 export const showLoading = createAction("[UI] Show Loading");
 
-export const hideLoading = createAction("[UI] Hide Loading")
+export const hideLoading = createAction("[UI] Hide Loading");
+
+export const showModal = createAction("[UI] Show Modal");
